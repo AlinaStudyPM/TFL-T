8bc2b81004c02001e6d352686d3eeebdd6281994
AlinaStudyPM <119669643+AlinaStudyPM@users.noreply.github.com>
2024-09-20 19:38:15 +0300

    Add tasks for practice1

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿String? text;
+HashSet<char> M = new HashSet<char>();
+Console.WriteLine("Введите текст: ");
+text = Console.ReadLine();
+CharsOfText(text, M);
+
+Console.Write("Количество различных символов в тексте: ");
+Console.WriteLine(M.Count());
+
+//Функция, составляющая на основе строки множество символов
+void CharsOfText(String? text, HashSet<Char> M)
+{
+    if(text != null)
+    {
+        foreach(char c in text)
+        {
+            M.Add(c);
+        }
+    }
+}
